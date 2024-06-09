d3e411f14d069b441fbc287a317aaf7ef2c99bf4
Igor Barbashov <bis200705@gmail.com>
2024-06-09 17:49:25 +0300

    feat: make some minor but visible changes

--- src/components/MainLayout/components/Header.tsx
+++ src/components/MainLayout/components/Header.tsx
@@ -33,7 +33,7 @@ export default function Header() {
             underline="none"
             to="/"
           >
-            My Store!
+            My Store! SOME MINOR NUT VISIBLE CHANGES!!!
           </Link>
         </Typography>
 
